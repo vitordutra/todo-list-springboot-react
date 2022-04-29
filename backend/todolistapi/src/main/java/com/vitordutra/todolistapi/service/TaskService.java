01b5ef5fafdcb204807148d3b75f53917258d28d
Vitor Dutra <jo.vitor.d@gmail.com>
2022-04-29 18:18:58 -0300

    Add findAllTasks method

--- backend/todolistapi/src/main/java/com/vitordutra/todolistapi/service/TaskService.java
+++ backend/todolistapi/src/main/java/com/vitordutra/todolistapi/service/TaskService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TaskService {
@@ -15,5 +17,9 @@ public class TaskService {
 
     public Task createTask(Task task) {
         return taskRepository.save(task);
+    }
+
+    public List<Task> listAllTasks() {
+        return taskRepository.findAll();
     }
 }
