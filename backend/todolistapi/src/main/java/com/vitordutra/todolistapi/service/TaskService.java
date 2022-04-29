978d6fb52e960fdd404b9c0eeb762b2a8b82ec54
Vitor Dutra <jo.vitor.d@gmail.com>
2022-04-29 17:49:57 -0300

    Add createTask

--- backend/todolistapi/src/main/java/com/vitordutra/todolistapi/service/TaskService.java
+++ backend/todolistapi/src/main/java/com/vitordutra/todolistapi/service/TaskService.java
@@ -1,4 +1,19 @@
 package com.vitordutra.todolistapi.service;
 
+import com.vitordutra.todolistapi.model.Task;
+import com.vitordutra.todolistapi.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
 }
