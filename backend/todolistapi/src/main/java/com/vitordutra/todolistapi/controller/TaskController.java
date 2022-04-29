c792e1ded8e999e112ad2778a1c1759e98f3fa10
Vitor Dutra <jo.vitor.d@gmail.com>
2022-04-29 18:14:24 -0300

    Add createTask endpoint

--- backend/todolistapi/src/main/java/com/vitordutra/todolistapi/controller/TaskController.java
+++ backend/todolistapi/src/main/java/com/vitordutra/todolistapi/controller/TaskController.java
@@ -1,4 +1,21 @@
 package com.vitordutra.todolistapi.controller;
 
+import com.vitordutra.todolistapi.model.Task;
+import com.vitordutra.todolistapi.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TaskController {
+
+    @Autowired
+    TaskService taskService;
+
+    @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
 }
