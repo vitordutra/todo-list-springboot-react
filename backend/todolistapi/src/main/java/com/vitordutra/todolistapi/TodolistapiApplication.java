eb3cd811845b3f5b442f44e0677ee19bb6c0c893
Vitor Dutra <jo.vitor.d@gmail.com>
2022-04-29 16:35:40 -0300

    Initialize using Spring Initialzr

---
+++ backend/todolistapi/src/main/java/com/vitordutra/todolistapi/TodolistapiApplication.java
@@ -0,0 +1,13 @@
+package com.vitordutra.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistapiApplication.class, args);
+	}
+
+}
