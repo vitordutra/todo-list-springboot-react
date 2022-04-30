e962644560481eb296f76a070cbca6a6792cc9de
Vitor Dutra <jo.vitor.d@gmail.com>
2022-04-30 11:25:19 -0300

    Change Date to LocalDateTime

--- backend/todolistapi/src/main/java/com/vitordutra/todolistapi/model/Task.java
+++ backend/todolistapi/src/main/java/com/vitordutra/todolistapi/model/Task.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
@@ -26,13 +26,13 @@ public class Task {
     private String description;
 
     @Column(nullable = false)
-    private Date deadLine;
+    private LocalDateTime deadLine;
 
     @CreationTimestamp
     @Column(name="created_at", nullable = false, updatable = false)
-    private Date createdAt;
+    private LocalDateTime createdAt;
 
     @UpdateTimestamp
     @Column(name="updated_at", nullable = false, updatable = false)
-    private Date updatedAt;
+    private LocalDateTime updatedAt;
 }
