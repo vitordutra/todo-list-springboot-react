eb9d06946075d18a9a1215a7c18cb611d7d4b1e9
Vitor Dutra <jo.vitor.d@gmail.com>
2022-05-02 13:58:55 -0300

    Change Box into a Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,80 @@
-import {
-  Checkbox,
-  Container,
-  Divider,
-  Fab,
-  IconButton,
-  Stack,
-  TextField,
-} from '@mui/material';
+import { Container, Fab, Stack, TextField } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/system';
 import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import DoneIcon from '@mui/icons-material/Done';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 import SelectAllIcon from '@mui/icons-material/SelectAll';
 
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import api from './services/api';
+import TaskList from './components/TaskList';
+import TaskItem from './components/TaskItem';
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  const [id, setId] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   useEffect(() => {
-    getTasks();
+    handleGetTasks();
   }, []);
 
-  async function getTasks() {
+  async function handleGetTasks() {
     try {
-      const taskData = await (
-        await axios.get('http://localhost:8080/api/v1/tasks')
-      ).data;
-      setTasks(taskData);
+      const taskData = await api.get('tasks');
+      const taskList = await taskData.data;
+      setTasks(taskList);
     } catch (error) {
       console.log(error);
     }
   }
 
+  function clearStates() {
+    setId('');
+    setTitle('');
+    setDescription('');
+  }
+
+  function fillStates(task) {
+    setId(task.id);
+    setTitle(task.title);
+    setDescription(task.description);
+  }
+
+  async function handleAddTask(event) {
+    event.preventDefault();
+
+    if (!title || !description) {
+      alert('Preencha todos os campos');
+    } else {
+      const body = {
+        title,
+        description,
+        done: false,
+      };
+
+      try {
+        await api.post('tasks', body);
+        alert('Tarefa cadastrada com sucesso');
+        clearStates();
+        handleGetTasks();
+      } catch (error) {
+        console.log(error);
+        alert('Erro ao adicionar Tarefa');
+      }
+    }
+  }
+
   return (
     <Container maxWidth='sm'>
-      <Box sx={{ my: 4 }}>
+      <Box
+        sx={{ my: 4 }}
+        component='form'
+        noValidate
+        autoComplete='off'
+        onSubmit={handleAddTask}>
         <Typography variant='h4' component='h1' gutterBottom>
           Todo List - Desafio CI&T
         </Typography>
@@ -53,90 +88,6 @@ function App() {
           </Fab>
         </Stack>
 
-        <Box
-          sx={{
-            border: 'solid 1px',
-            marginTop: 2,
-            borderColor: 'rgba(0, 0, 0, 0.12)',
-            borderRadius: 2,
-          }}>
-          <Stack
-            direction='row'
-            spacing={2}
-            alignItems='center'
-            justifyContent='space-between'>
-            <Checkbox />
-            <Stack alignItems='center'>
-              <Typography>Texto de Teste</Typography>
-              <Typography variant='caption'>Descrição</Typography>
-            </Stack>
-
-            <Stack
-              direction='row'
-              alignItems='center'
-              justifyContent='space-between'>
-              <IconButton>
-                <EditIcon />
-              </IconButton>
-              <IconButton>
-                <DeleteIcon />
-              </IconButton>
-              <IconButton>
-                <DoneIcon />
-              </IconButton>
-            </Stack>
-          </Stack>
-          <Divider />
-
-          <Stack
-            direction='row'
-            spacing={2}
-            alignItems='center'
-            justifyContent='space-between'>
-            <Checkbox />
-            <Typography>Texto de Teste</Typography>
-            <Stack
-              direction='row'
-              alignItems='center'
-              justifyContent='space-between'>
-              <IconButton>
-                <EditIcon />
-              </IconButton>
-              <IconButton>
-                <DeleteIcon />
-              </IconButton>
-              <IconButton>
-                <DoneIcon />
-              </IconButton>
-            </Stack>
-          </Stack>
-          <Divider />
-
-          <Stack
-            direction='row'
-            spacing={2}
-            alignItems='center'
-            justifyContent='space-between'>
-            <Checkbox />
-            <Typography>Texto de Teste</Typography>
-            <Stack
-              direction='row'
-              alignItems='center'
-              justifyContent='space-between'>
-              <IconButton>
-                <EditIcon />
-              </IconButton>
-              <IconButton>
-                <DeleteIcon />
-              </IconButton>
-              <IconButton>
-                <DoneIcon />
-              </IconButton>
-            </Stack>
-          </Stack>
-          <Divider />
-        </Box>
-
         <Box sx={{ marginTop: 2 }}>
           <Stack spacing={2} direction='row' justifyContent='space-around'>
             <Fab color='primary' aria-label='add' variant='extended'>
@@ -149,6 +100,16 @@ function App() {
             </Fab>
           </Stack>
         </Box>
+
+        <TaskList>
+          {tasks?.map(task => (
+            <TaskItem
+              key={task.id}
+              title={task.title}
+              description={task.description}
+            />
+          ))}
+        </TaskList>
       </Box>
     </Container>
   );
