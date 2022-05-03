6084dcae5e483e39b0d78931374ffcfdc00b7201
Vitor Dutra <jo.vitor.d@gmail.com>
2022-05-03 11:26:57 -0300

    Finish Project

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1/',
+  baseURL: 'http://localhost:8080/api/v1',
+  //baseURL: 'https://desolate-plains-22853.herokuapp.com/api/v1',
 });
 
 export default api;
